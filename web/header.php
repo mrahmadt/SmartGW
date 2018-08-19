29255bee78461329c7e7143f58e48b6127d5a4ca
Aft <ahmadt@gmail.com>
2018-08-19 06:42:38 +0300

    Update header.php

--- web/header.php
+++ web/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-0 pl-2">
-  <a class="navbar-brand" href="index.php">Smart Gateways</a>
+  <a class="navbar-brand" href="index.php">Smart Gateway</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
