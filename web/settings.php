599f8445bd42780a71163465bb619b14a055bc44
mrahmadt <ahmadt@gmail.com>
2018-08-21 03:01:09 +0300

    dns provider and logs

--- web/settings.php
+++ web/settings.php
@@ -12,6 +12,19 @@ while ( $row = $ret->fetchArray() ) {
 }
 $db->close();
 
+$dnsips = [];
+if(isset($settings['dns1ip']['value'])){
+	$dnsips[] = $settings['dns1ip']['value'];
+}
+if(isset($settings['dns2ip']['value'])){
+	$dnsips[] = $settings['dns2ip']['value'];
+}
+$dnsips = implode(',',$dnsips);
+if($dnsips==''){
+	$dnsips = '208.67.222.222,208.67.220.220';
+}
+
+
 if($_SERVER['REQUEST_METHOD']  == 'POST'){
 	$db = new SQLite3(DATABASE_FILE);
 	if(!$db) {
@@ -20,7 +33,17 @@ if($_SERVER['REQUEST_METHOD']  == 'POST'){
 	}
 	$stmtInsert = $db->prepare('INSERT INTO settings (name,value) VALUES(:name,:value)');
 	$stmtUpdate = $db->prepare('UPDATE settings SET value=:value WHERE name LIKE :name');
-	foreach($_POST as $name=>$value){
+
+	$myPOST = $_POST;
+	$checkBoxes = ['disablesquidlog'=>0,'disablesniproxylog'=>0,'disablednsquerylog'=>0];
+
+	foreach($checkBoxes as $name=>$value){
+		if(!isset($_POST[$name])){
+			$myPOST[$name] = $value;
+		}
+	}
+
+	foreach($myPOST as $name=>$value){
 		$value = trim($value);
 		if(isset($settings[$name])){
 			$stmtUpdate->bindValue(':name', $name);
@@ -32,13 +55,28 @@ if($_SERVER['REQUEST_METHOD']  == 'POST'){
 			$stmtInsert->bindValue(':value',$value);
 			$stmtInsert->execute();
 		}
-	}
+	}	
     $db->close();
 	
-	UpdateDNSMasqConf();
-	if(isset($settings['vpncountrycode'])){
-		$exeout = [];
-		exec('/usr/bin/sudo /usr/local/bin/openpyn -d '. $settings['vpncountrycode']['value'], $exeout, $return);
+
+//	if(isset($myPOST['updateupstreamdns']) && $myPOST['updateupstreamdns'] == "1"){
+	if($myPOST['disablesquidlog']==1) { $access_log=false; }else{ $access_log=true; }
+	UpdateSquidConf($myPOST['dns1ip'],$myPOST['dns2ip'],$access_log);
+	
+	if($myPOST['disablednsquerylog']==1) { $access_log=false; }else{ $access_log=true; }
+	UpdateDNSMasqConf($myPOST['dns1ip'],$myPOST['dns2ip'],$access_log);
+	
+	if($myPOST['disablesniproxylog']==1) { $access_log=false; }else{ $access_log=true; }
+	UpdateSNIProxyConf($myPOST['dns1ip'],$myPOST['dns2ip'],$access_log);
+
+	if(isset($myPOST['updatednsdomains']) && $myPOST['updatednsdomains'] == "1"){
+		UpdateDNSMasqDomains();
+	}
+	if(isset($myPOST['restartvpn']) && $myPOST['restartvpn'] == "1"){
+		if(isset($settings['vpncountrycode'])){
+			$exeout = [];
+			exec('/usr/bin/sudo /usr/local/bin/openpyn -d '. $settings['vpncountrycode']['value'], $exeout, $return);
+		}
 	}
 	header('Location: settings.php');
 	exit;
@@ -61,24 +99,26 @@ if ($return == 0) {
 }
 
 $exeout = [];
-exec('openpyn -l', $exeout, $return);
 
-$vpncountries = [];
-if ($return == 0) {
-	foreach ($exeout as $row){
-		$row = explode('      ',$row);
-		$country = explode(':',$row[0]);
-		$country = trim($country[1]);
-		$countrycode = explode(':',$row[1]);
-		$countrycode = trim($countrycode[1]);
-		$vpncountries[trim($country)] = trim($countrycode);
+if(!file_exists('/usr/local/bin/openpyn')) {
+	exec('openpyn -l', $exeout, $return);
+
+	$vpncountries = [];
+	if ($return == 0) {
+		foreach ($exeout as $row){
+			$row = explode('      ',$row);
+			$country = explode(':',$row[0]);
+			$country = trim($country[1]);
+			$countrycode = explode(':',$row[1]);
+			$countrycode = trim($countrycode[1]);
+			$vpncountries[trim($country)] = trim($countrycode);
+		}
+	}else{
+		echo '<h3>ERROR</h3>';
+		print_r($return);
+		exit;
 	}
-}else{
-	echo '<h3>ERROR</h3>';
-	print_r($return);
-	exit;
 }
-
 
 
 
@@ -99,7 +139,30 @@ require_once 'header.php';
 	  Static IP is needed, the IP of this interface will be your new DNS IP, make sure to change your DNS IP in your internet router to this IP.
 	</small>
   </div>
-  
+ 
+ <div class="form-group">
+   <label for="UpstreamDNSProvidersInput">Upstream DNS Provider</label>
+   <select class="form-control" id="UpstreamDNSProvidersInput" name=UpstreamDNSProvider>
+ <?php foreach($UpstreamDNSProviders as $name => $value){ ?>
+ <option data-dnsips="<?php echo implode(',',$value);?>" value="<?php echo $name;?>" <?php if(isset($settings['UpstreamDNSProvider']) && $settings['UpstreamDNSProvider']['value']==$name){?>selected<?php }?>><?php echo $name;?></option>
+ <?php } ?>
+ <option data-dnsips="<?php echo $dnsips;?>" value="custom" <?php if(isset($settings['UpstreamDNSProvider']) && $settings['UpstreamDNSProvider']['value']=='custom'){?>selected<?php }?>>Custom</option>
+ 
+   </select>
+   <small>Please <a href="https://github.com/mrahmadt/SmartGW/wiki/Upstream-DNS-Providers" target=_blank>click here</a> for more information</small> 
+ </div>
+ 
+ <div id="DNSIPsInput" <?php if(isset($settings['UpstreamDNSProvider']) && $settings['UpstreamDNSProvider']['value']!='custom'){?>style="display:none;"<?php }?>>
+   <div class="form-group">
+     <label for="dns1ip">Primary DNS Server:</label>
+	 <input type="text" class="form-control" id="dns1ip" name="dns1ip" placeholder="103.86.96.100">
+   </div>
+   <div class="form-group">
+     <label for="dns2ip">Secondary DNS Server:</label>
+	 <input type="text" class="form-control" id="dns2ip" name="dns2ip" placeholder="103.86.99.100">
+   </div>
+ </div>
+ 
 <div class="form-group">
   <label for="vpncountryinput">VPN Country</label>
   <select class="form-control" id="vpncountryinput" name=vpncountrycode>
@@ -108,10 +171,82 @@ require_once 'header.php';
 <?php } ?>
   </select>
 </div>
-  
+<div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="disableSquidLog" name="disablesquidlog" value="1" <?php if(isset($settings['disablesquidlog']) && $settings['disablesquidlog']['value']=="1"){?>checked<?php } ?>>
+<label class="form-check-label" for="disableSquidLog">Disable Squid access log</label>
+</div>
+<div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="disablesniproxylog" name="disablesniproxylog" value="1" <?php if(isset($settings['disablesniproxylog']) && $settings['disablesniproxylog']['value']=="1"){?>checked<?php } ?>>
+<label class="form-check-label" for="disablesniproxylog">Disable SNIProxy access log</label>
+</div>
+<div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="disablednsquerylog" name="disablednsquerylog" value="1" <?php if(isset($settings['disablednsquerylog']) && $settings['disablednsquerylog']['value']=="1"){?>checked<?php } ?>>
+<label class="form-check-label" for="disablednsquerylog">Disable DNS query log</label>
+</div>
+
+<div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="updatednsdomains" name="updatednsdomains" value="1">
+<label class="form-check-label" for="updatednsdomains">Restart my DNS</label>
+</div>
+<!--div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="updateupstreamdns" name="updateupstreamdns" value="1">
+<label class="form-check-label" for="updateupstreamdns">Apply my upstream DNS</label>
+</div-->
+
+<div class="form-group form-check">
+<input type="checkbox" class="form-check-input" id="restartvpn" name="restartvpn" value="1">
+<label class="form-check-label" for="restartvpn">Restart my VPN</label>
+</div>
+ 
   
   <button type="submit" class="btn btn-primary mb-2">Submit</button>
 </form>
 
 
 <?php require_once 'footer.php';?>
+<script>
+$( document ).ready(function() {
+	var firstChange = true;
+	$( "#interfaceinput" ).change(function() {
+		$('#updatednsdomains').prop('checked', true);
+	});
+	$( "#vpncountryinput" ).change(function() {
+		$('#restartvpn').prop('checked', true);
+	});
+	
+	$( "#dns1ip" ).on('input',function(e){
+		$('#updateupstreamdns').prop('checked', true);
+	});
+	$( "#dns2ip" ).on('input',function(e){
+		$('#updateupstreamdns').prop('checked', true);
+	});
+	
+		$( "#UpstreamDNSProvidersInput" ).change(function() {
+			if(firstChange==true){
+				firstChange = false;
+			}else{
+				$('#updateupstreamdns').prop('checked', true);
+			}
+			selected = $("#UpstreamDNSProvidersInput option:selected");
+			if(selected.text()=='Custom'){
+				$('#DNSIPsInput').fadeIn();
+			}else{
+				$('#DNSIPsInput').fadeOut();
+			}
+			var attr = selected.attr('data-dnsips');
+			if (typeof attr !== typeof undefined && attr !== false) {
+				ips = attr.split(","); 
+				if(ips.length>0){
+					$('#dns1ip').val(ips[0]);
+				}
+				if(ips.length>1){
+					$('#dns2ip').val(ips[1]);
+				}
+			}else{
+				$('#dns1ip').val('');
+				$('#dns2ip').val('');
+			}
+		}).change();
+});
+</script>
+
